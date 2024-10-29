f9a87b14f508092bc2ea92f15041fd320d3e18aa
yulin <yulinliu@rrc.ca>
2024-10-29 10:28:20 -0500

    complete the pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     # Add Pagination to Collection Navigation
-    # @cities = City.page(params[:page]).per(10) # This should now work
+    # @cities = City.page(params[:page]).per(10)
     @health_data = HealthData.page(params[:page]).per(10) # This too
     @cities = City.paginate(page: params[:page], per_page: 10)
 
