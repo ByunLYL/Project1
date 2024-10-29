d32244652e392098b63fb30bef4ae8cc5fd31425
yulin <yulinliu@rrc.ca>
2024-10-29 10:33:29 -0500

    start 4 searching and filtering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     # Add Pagination to Collection Navigation
     @cities = City.paginate(page: params[:page], per_page: 10)
 
-    @health_data = HealthData.page(params[:page]).per(10) # This too
+    @health_data = HealthData.page(params[:page]).per(10)
 
 
   end
