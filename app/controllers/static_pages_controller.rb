31dfa159f15fa58300be7c567592f5fcefae7b7d
yulin <yulinliu@rrc.ca>
2024-10-29 10:47:19 -0500

    complete the 4.1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
     # Add Pagination to Collection Navigation
+    if params[:query].present?
+      @cities = City.where("name ILIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
+  else
     @cities = City.paginate(page: params[:page], per_page: 10)
-
+  end
     @health_data = HealthData.page(params[:page]).per(10)
 
 
