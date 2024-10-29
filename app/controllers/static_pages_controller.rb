1b1fbb954b0a5381b457884a1e6600a9bed27b06
yulin <yulinliu@rrc.ca>
2024-10-29 10:58:15 -0500

    fixed the 4.1, search button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-    # Add Pagination to Collection Navigation
     if params[:query].present?
-      @cities = City.where("name ILIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
-  else
-    @cities = City.paginate(page: params[:page], per_page: 10)
-  end
-    @health_data = HealthData.page(params[:page]).per(10)
+      # 使用 LOWER 以便在 SQLite 中实现不区分大小写的搜索
+      @cities = City.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%").paginate(page: params[:page], per_page: 10)
+    else
+      @cities = City.paginate(page: params[:page], per_page: 10)
+    end
 
-
+    @health_data = HealthData.paginate(page: params[:page], per_page: 10)
   end
 end
-
